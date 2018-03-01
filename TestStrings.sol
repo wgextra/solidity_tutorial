78967b8a616537a2db9da313d752e4646621584b
Takumi Kumagai <kumagai.takumi1223@gmail.com>
2018-03-01 13:56:45 +0900

    added files for tutorial 7

---
+++ TestStrings.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.20;
+
+import "browser/Strings.sol";
+
+contract TestStrings{
+    using Strings for string;
+    function testConcat(string _base) returns (string){
+        return _base.concat("_suffix");
+    }
+
+    function needleInHaystack(string _base) returns (int){
+        return _base.strpos("t");
+    }
+}
