f77c34e2418c4fef1e9f9e248429bbd0b4e86e9b
Takumi Kumagai <kumagai.takumi1223@gmail.com>
2018-03-01 13:27:43 +0900

    added file for tutorial 6

---
+++ dataTypes.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.4.0;
+
+contract DataTypes{
+    bool myBool = false;
+    int8 myInt = -128;
+    uint8 myUint = 255;
+
+    string myString;
+    uint8[] myStringArr;
+
+    byte myValue;
+    bytes1 myBytes1;
+    bytes32 myBytes32;
+
+    // fixed256x8 myFixed = 1.0;
+
+    enum Action {ADD,REMOVE,UPDATE}
+    Action myAction = Action.ADD;
+
+    address myAddress;
+
+    function assignAddress(){
+        myAddress = msg.sender;
+        myAddress.balance;
+        myAddress.transfer(10);
+    }
+
+    uint[] myIntArr = [1,2,3];
+
+    function arrFunc(){
+        myIntArr.push(1);
+        myIntArr.length;
+        myIntArr[0];
+    }
+
+    uint[10] myFixedArr;
+
+    struct Account{
+        uint balance;
+        uint dailyLimit;
+    }
+
+    Account myAccount;
+
+    function structFunc(){
+        myAccount.balance = 100;
+    }
+
+    mapping (address => Account) _account;
+
+    function () payable{
+        _account[msg.sender].balance += msg.value;
+    }
+
+    function getBalance() returns (uint){
+        return _account[msg.sender].balance;
+    }
+
+}
